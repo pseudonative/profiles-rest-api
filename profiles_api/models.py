380da71f7f59db0c027b3e9e0b112ba7f98ddf90
Jeremy Coando <jaycoan@yahoo.com>
2020-11-22 12:53:51 -0700

    added to models

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    def create_user(self, email, password=None):
+    def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError('User must have an email address')

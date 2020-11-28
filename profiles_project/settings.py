47974ffc9712ca4ffdfe5127639a141b2b6960aa
Jeremy Coando <jaycoan@yahoo.com>
2020-11-28 08:08:17 -0700

    Added permissions to run bash scripts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j7#kl=4)j6kq)(7bf$akvjgwe26d8jrsy7p0kd0a+!685x(m20'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
